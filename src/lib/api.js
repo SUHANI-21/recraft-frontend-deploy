4cbc847fa88ac1e0b4d9ff2e1c51c8cf6838857d
SUHANI-21 <2023ci_suhani_b@nie.ac.in>
2025-11-18 05:43:10 +0530

    switch to env variable

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,6 @@
 // This is the base URL of your running backend server
-const API_URL = 'http://localhost:5001';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 
 /**
  * A helper function to handle API responses consistently.
